6a8bf125f102e0a4fdf1fbbb28b9183a690ecd62
agusbhb <abeltrandehere004@ikasle.ehu.eus>
2025-12-02 22:34:48 +0100

    Subir Proyecto RidesJSF 1era version

---
+++ src/main/java/model/IndexBean.java
@@ -0,0 +1,12 @@
+package model;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+
+@Named("index")
+@ApplicationScoped
+public class IndexBean implements Serializable {
+
+}
